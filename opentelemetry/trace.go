9f2b0e12ccaeac8f990113a806a1882d0f454e5f
booscaaa <boscardinvinicius@gmail.com>
2025-03-19 09:09:37 -0300

    :sparkles: feat(opentelemetry): adiciona middleware HTTP para rastreamento automático de requisições

--- opentelemetry/trace.go
+++ opentelemetry/trace.go
@@ -2,10 +2,21 @@ package opentelemetry
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel/trace"
 )
 
 func StartTracing(ctx context.Context, name string) (context.Context, trace.Span) {
 	return openTelemetryConfig.tracer.Start(ctx, name)
+}
+
+func HttpMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		verb := request.Method
+		path := request.URL.Path
+		otelhttp.NewHandler(next, fmt.Sprintf("%s %s", verb, path)).ServeHTTP(response, request)
+	})
 }
